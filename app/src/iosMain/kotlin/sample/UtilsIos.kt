c8afdcd03be1e49d71fb1c6ef157108b73449e49
rudolfhladik <hladik.rudolf@gmail.com>
2020-10-07 15:48:12 +0200

    fix backing field error

--- app/src/iosMain/kotlin/sample/UtilsIos.kt
+++ app/src/iosMain/kotlin/sample/UtilsIos.kt
@@ -8,8 +8,7 @@ import kotlin.native.concurrent.SharedImmutable
 import kotlin.native.concurrent.ensureNeverFrozen
 
 @SharedImmutable
-actual val mainUiContext: CoroutineContext
-    get() = Dispatchers.Main
+actual val mainUiContext: CoroutineContext = Dispatchers.Main
 
 @SharedImmutable
 actual val workerDispatcher: CoroutineDispatcher = newSingleThreadContext("mr background")
